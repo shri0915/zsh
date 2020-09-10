0f272f31b1aac4ab64ad46f18f74f0e830c81c9e
shri0915 <shrijit.panicker@gmail.com>
2020-09-11 04:47:23 +0530

    Good to go

--- zshtheme.sh
+++ zshtheme.sh
@@ -4,7 +4,7 @@ sudo apt install zsh
 echo "When it asks for what to run default instead of ***/bin/bash*** please select ***/bin/zsh*** without the stars (*)"
 chsh $USER
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/powerlevel10k
-echo 'source /home/$USER/powerlevel10k/powerlevel10k.zsh-theme' >>! /home/$USER/.zshrc
+echo 'source /home/$USER/powerlevel10k/powerlevel10k.zsh-theme' >> /home/$USER/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source /home/$USER/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
