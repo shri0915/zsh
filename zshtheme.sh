70cc84cfe61e5d7a0bc9efb62870da8ea9c48515
shri0915 <shrijit.panicker@gmail.com>
2020-09-11 04:38:25 +0530

    Few more changes for testing

--- zshtheme.sh
+++ zshtheme.sh
@@ -3,8 +3,8 @@ echo "Installing ZSH instead of bash"
 sudo apt install zsh
 echo "When it asks for what to run default instead of ***/bin/bash*** please select ***/bin/zsh*** without the stars (*)"
 chsh $USER
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/powerlevel10k
+echo 'source /home/$USER/powerlevel10k/powerlevel10k.zsh-theme' >>! /home/$USER/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "source /home/$USER/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
